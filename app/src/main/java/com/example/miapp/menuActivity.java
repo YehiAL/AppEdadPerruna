1a17228768d51f3d0ce3a49039fa7fa7cae355b2
YehiAL <arayayehi01@gmail.com>
2024-09-07 12:35:11 -0400

    menu a perro

--- app/src/main/java/com/example/miapp/menuActivity.java
+++ app/src/main/java/com/example/miapp/menuActivity.java
@@ -1,6 +1,10 @@
 package com.example.miapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +14,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class menuActivity extends AppCompatActivity {
 
+    private Button btnPerro;
+    private TextView lblnombre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        lblnombre = (TextView)findViewById(R.id.lblNombre);
+        btnPerro = (Button) findViewById(R.id.idPerro);
+        Intent intent = getIntent();
+        String nombre = intent.getExtras().getString("nombre");
 
+        lblnombre.setText("Bienvenido " + nombre);
+
+        btnPerro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                explicito();
+            }
+        });
+    }
+
+    public void explicito(){
+        Intent intent = new Intent(this,PerroActivity.class);
+        startActivity(intent);
     }
 }
